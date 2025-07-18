ce7c6480fedb5e486a6839d82fe26690822bd4e0
Tajbakhsh <kiarash.tajbakhsh@empa.ch>
2025-07-18 12:07:00 +0200

    Example input added

--- download_model.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import snapshot_download
-
-snapshot_download(
-    repo_id="HippoCanFly/ct-thyroid-classifier",
-    repo_type="model",
-    local_dir="/app/hf_model",
-    token="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"
-)
